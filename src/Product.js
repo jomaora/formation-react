d7fd66d8c7f1a1df81ae1a3ae6e4bdbbb3c84ef0
Joan Ortega <jomaora@gmail.com>
2019-04-18 11:33:02 +0200

    product list dynamic

--- src/Product.js
+++ src/Product.js
@@ -1,22 +1,33 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-export default (props) => {
+const Product = (props) => {
 	const {product} = props;
+	const {id, image, title, price} = product;
 	return (
-		<div className="col s3">
-			<div className="card">
-				<div className="card-image">
-					<img src={product.image} alt="" />
-					<a className="btn-floating btn-large halfway-fab waves-effect waves-light red" onClick={() => {console.log('TODO ADD', product.id)}}>
-						<i className="material-icons">add_shopping_cart</i>
-					</a>
-				</div>
-				<div className="card-content">
-					<span className="card-title">{product.title} ({product.price} €)</span>
-				</div>
+		<div className="card">
+			<div className="card-image">
+				<img src={image} alt="" />
+				<a className="btn-floating btn-large halfway-fab waves-effect waves-light red" onClick={() => {console.log('TODO ADD', id)}}>
+					<i className="material-icons">add_shopping_cart</i>
+				</a>
+			</div>
+			<div className="card-content">
+				<span className="card-title">{title} ({price} €)</span>
 			</div>
 		</div>
 	);
-}
+};
+
+Product.propTypes = {
+	product: PropTypes.shape({
+		id: PropTypes.string.isRequired,
+		image: PropTypes.string.isRequired,
+		title: PropTypes.string.isRequired,
+		price: PropTypes.number.isRequired
+	}).isRequired
+};
+
+export default Product;
